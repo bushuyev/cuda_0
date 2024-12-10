f77832efbffcdab78f70fbb1335aaf4d2e4da463
Yevgen Bushuyev <yevgen.bushuyev@gmail.com>
2024-12-10 18:54:14 +0100

    cuda 0

---
+++ test0.cu
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+__global__ void testKernel(int val) {
+
+  val *= 20;
+  printf("vall=%d\n", val);
+}
+
+int main(int argc, char **argv) {
+  cudaSetDevice(0); // Ensure device is set
+
+  printf("printf() is called. Output:\n\n");
+
+  dim3 dimGrid(1, 1);
+  dim3 dimBlock(1, 1, 1);
+  testKernel<<<dimGrid, dimBlock>>>(10);
+  cudaError_t err = cudaDeviceSynchronize();
+  if (err != cudaSuccess) {
+    fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(err));
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
